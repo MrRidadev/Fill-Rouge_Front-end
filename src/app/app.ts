b8aae44b342523d8db791c6612efbbd92032f90d
ridaDev <kiratterida@gmail.com>
2025-07-02 17:21:39 +0100

    structure de front end

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './pages/navbar/navbar';
+import {HeroSection} from './pages/hero-section/hero-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, HeroSection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
