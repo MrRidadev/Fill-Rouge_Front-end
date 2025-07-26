dfe25e69503a67ea1509dc640c5c1c8759a31dc3
ridaDev <kiratterida@gmail.com>
2025-07-26 14:27:30 +0100

    fix erreur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {Login} from './pages/login/login';
+import {Register} from './pages/register/register';
+import {HeroSection} from './pages/hero-section/hero-section';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroSection
+  },
+  {
+    path: 'login',
+    component : Login
+  },
+  {
+    path: 'register',
+    component : Register
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Navbar} from './pages/navbar/navbar';
 import {HeroSection} from './pages/hero-section/hero-section';
+import {Login} from './pages/login/login';
+import {Register} from './pages/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, HeroSection],
+  imports: [RouterOutlet, Navbar, HeroSection, Login, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
