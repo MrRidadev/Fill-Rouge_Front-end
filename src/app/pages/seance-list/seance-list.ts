546fda954c2003d190829fb0d959e8dad4c6d7d6
ridaDev <kiratterida@gmail.com>
2025-08-30 15:29:33 +0100

    get seance à séance-list

--- src/app/pages/seance-list/seance-list.ts
+++ src/app/pages/seance-list/seance-list.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Seance} from '../../services/seance';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-seance-list',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './seance-list.html',
   styleUrl: './seance-list.css'
 })
-export class SeanceList {
+export class SeanceList implements OnInit {
+
+  seances: Seance[] = [];
+
+  constructor(private seanceService : Seance) {}
+
+  ngOnInit() {
+    this.loadSeances();
+  }
+
+  loadSeances() {
+    this.seanceService.getSeance().subscribe((data) => {
+      this.seances = data;
+    })
+  }
 
 }
