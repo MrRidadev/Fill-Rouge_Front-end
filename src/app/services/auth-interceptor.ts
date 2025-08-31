294929687bdfd9201f6996375ff8d422f6ebc58b
ridaDev <kiratterida@gmail.com>
2025-08-31 15:12:57 +0100

    créer AuthInterceptor

---
+++ src/app/services/auth-interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser') || 'null');
+    const token = currentUser?.token; // si ton LoginResponse renvoie token
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
