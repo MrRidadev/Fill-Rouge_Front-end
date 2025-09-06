803b92445f0aea0eb49c31803a2deb262d4e10eb
ridaDev <kiratterida@gmail.com>
2025-09-06 16:31:35 +0100

    créer fonction getHeaders avec get et post au salle

--- src/app/services/salle.ts
+++ src/app/services/salle.ts
@@ -1,39 +1,42 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth';
 
 
 export interface Salle{
-  id : number;
+  id?: number;
   nom : string;
   capacite : number;
 }
 @Injectable({
   providedIn: 'root'
 })
-export class Salle {
+export class SalleService  {
 
   private apiUrl = 'http://localhost:8087/salle';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private auth: AuthService) { }
 
   // Fonction pour récupérer le token depuis le localStorage
-  private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('token'); // Ton token JWT stocké
+  private getHeaders() {
+    const token = this.auth.getToken();
+    console.log("salle: "+token);
     return new HttpHeaders({
-      'Authorization': `Bearer ${token}`
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
     });
   }
 
-  // GET toutes les salles
+  // afficher toutes les salles
   getAllSalles(): Observable<Salle[]> {
     return this.http.get<Salle[]>(`${this.apiUrl}/getAll`);
   }
 
-  // POST ajouter une salle
+  // ajouter une salle
   addSalle(salle: Salle): Observable<Salle> {
     return this.http.post<Salle>(`${this.apiUrl}/addSalle`, salle, {
-      headers: this.getAuthHeaders()
+      headers: this.getHeaders()
     });
   }
 }
