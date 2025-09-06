d041d499ac990860e872c16239122d6bcc214f30
ridaDev <kiratterida@gmail.com>
2025-09-06 15:51:13 +0100

    créer fonction ajouter et afficher salle

--- src/app/services/salle.ts
+++ src/app/services/salle.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 
 export interface Salle{
@@ -11,5 +13,27 @@ export interface Salle{
 })
 export class Salle {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8087/salle';
+
+  constructor(private http: HttpClient) { }
+
+  // Fonction pour récupérer le token depuis le localStorage
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // Ton token JWT stocké
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+  // GET toutes les salles
+  getAllSalles(): Observable<Salle[]> {
+    return this.http.get<Salle[]>(`${this.apiUrl}/getAll`);
+  }
+
+  // POST ajouter une salle
+  addSalle(salle: Salle): Observable<Salle> {
+    return this.http.post<Salle>(`${this.apiUrl}/addSalle`, salle, {
+      headers: this.getAuthHeaders()
+    });
+  }
 }
